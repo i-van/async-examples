f81a9dfd98bdf33dbf4d1ca2eed84465065c0b8e
Ivan Nosov <i.k.nosov@gmail.com>
2014-10-13 14:45:50 +0300

    promisify methods

--- async_array.js
+++ async_array.js
@@ -1,4 +1,6 @@
 
+var Q = require('q');
+
 /**
  * @param {Array} [data]
  * @constructor
@@ -9,53 +11,80 @@ function AsyncAray(data) {
 
 /**
  * @param {string} index
- * @param {Function} done
+ * @param {Function} [done]
+ * @returns {Q.promise}
  */
 AsyncAray.prototype.getByIndex = function(index, done) {
-    var _this = this;
+    var _this = this,
+        deferred = Q.defer();
+    done || (done = function() {});
 
     process.nextTick(function() {
-        done(null, _this._data[index]);
+        var res = _this._data[index];
+        deferred.resolve(res);
+        done(null, res);
     });
+
+    return deferred.promise;
 };
 
 /**
- * @param {Function} done
+ * @param {Function} [done]
+ * @returns {Q.promise}
  */
 AsyncAray.prototype.getLength = function(done) {
-    var _this = this;
+    var _this = this,
+        deferred = Q.defer();
+    done || (done = function() {});
 
     process.nextTick(function() {
-        done(null, _this._data.length);
+        var res = _this._data.length;
+        deferred.resolve(res);
+        done(null, res);
     });
+
+    return deferred.promise;
 };
 
 /**
  * @param {number} i
  * @param {number} j
- * @param {Function} done
+ * @param {Function} [done]
+ * @returns {Q.promise}
  */
 AsyncAray.prototype.swap = function(i, j, done) {
-    var _this = this;
+    var _this = this,
+        deferred = Q.defer();
+    done || (done = function() {});
 
     process.nextTick(function() {
         var temp = _this._data[i];
         _this._data[i] = _this._data[j];
         _this._data[j] = temp;
 
+        deferred.resolve();
         done();
     });
+
+    return deferred.promise;
 };
 
 /**
- * @param {Function} done
+ * @param {Function} [done]
+ * @returns {Q.promise}
  */
 AsyncAray.prototype.getData = function(done) {
-    var _this = this;
+    var _this = this,
+        deferred = Q.defer();
+    done || (done = function() {});
 
     process.nextTick(function() {
-        done(null, _this._data);
+        var res = _this._data;
+        deferred.resolve(res);
+        done(null, res);
     });
+
+    return deferred.promise;
 };
 
 module.exports = AsyncAray;
